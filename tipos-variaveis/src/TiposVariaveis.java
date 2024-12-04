3302ab35329fb7da8b889a151fd41420b7ec6ccc
Alessandro Ribeiro <162641601+AleRCJr@users.noreply.github.com>
2024-12-04 12:56:47 -0300

    Delete tipos-variaveis directory

--- tipos-variaveis/src/TiposVariaveis.java
+++
@@ -1,9 +0,0 @@
-public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        final  String CPF = "75005638172";
-
-        System.out.println(CPF);
-
-        
-    }
-}
